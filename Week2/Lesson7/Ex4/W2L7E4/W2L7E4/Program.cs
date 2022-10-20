b5770def12195da1b5e61c6a2dc22ffba47480d6
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 22:23:33 +0200

    W2L7Ex4 - gotowe

---
+++ Week2/Lesson7/Ex4/W2L7E4/W2L7E4/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Napisz program w C#, który sprawdzi czy podany przez użytkownika rok jest rokiem przestępnym.
+Dane testowe : 2016
+Rezultat w terminalu :
+2016 jest rokiem przestępnym
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok");
+            int year = int.Parse(Console.ReadLine());
+            if (((year%4==0) && (year%100!=0)) || (year%400==0))
+            {
+                Console.WriteLine($"Rok {year} jest rokiem przestępnym");
+            }
+            else
+            {
+                Console.WriteLine($"Rok {year} nie jest rokiem przestępnym");
+            }            
+        }
+    }
+}
