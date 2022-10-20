1aa1024a53e002a7ceef6569c6ef99d90be85392
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 22:09:42 +0200

    W2L7Ex2 - gotowe

---
+++ Week2/Lesson7/Ex2/W2L7E2/W2L7E2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest parzysta czy nieparzysta.
+Dane testowe : 15
+Rezultat w terminalu :
+15 jest liczbą nieparzystą
+ */
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a%2 == 0)
+            {
+                Console.WriteLine($"Liczba {a} jest liczbą parzystą ");
+            }
+            else
+            {
+                Console.WriteLine($"Liczba {a} jest liczbą nieparzystą ");
+            }
+            
+        }
+    }
+}
