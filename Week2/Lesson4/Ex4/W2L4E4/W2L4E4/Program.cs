a30d0cfb2c14ca85675bdd861d3a39cb9072704d
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 16:04:58 +0200

    W2L4Ex5- gotowe

--- Week2/Lesson4/Ex4/W2L4E4/W2L4E4/Program.cs
+++ Week2/Lesson4/Ex4/W2L4E4/W2L4E4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L4E4            
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            string b = "Szkoła Dotneta";
+            double c = 12.5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
