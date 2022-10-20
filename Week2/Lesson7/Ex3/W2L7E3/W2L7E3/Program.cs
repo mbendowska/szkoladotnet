515fa32d7e453825d4b99d5420738af1315cca0d
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 22:15:58 +0200

    W2L7Ex3 - gotowe

---
+++ Week2/Lesson7/Ex3/W2L7E3/W2L7E3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest dodatnia czy ujemna.
+Dane testowe : 14
+Rezultat w terminalu :
+14 jest liczbą dodatnią
+*/
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            double a = double.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine($"liczba {a} jest liczbą dodatnią");
+            }
+            else if (a < 0)
+                 {
+                    Console.WriteLine($"liczba {a} jest liczbą ujemną");
+                 }
+                else Console.WriteLine($"liczba {a} jest zerem");
+        }
+    }
+}
