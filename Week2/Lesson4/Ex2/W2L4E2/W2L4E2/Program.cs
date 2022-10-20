d5ba35dec70694038b851d07ba55c9820684e52d
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 15:57:51 +0200

    W2L4Ex2 - gotowe

--- Week2/Lesson4/Ex2/W2L4E2/W2L4E2/Program.cs
+++ Week2/Lesson4/Ex2/W2L4E2/W2L4E2/Program.cs
@@ -7,9 +7,9 @@ namespace W2L4Ex2
     {
         public static void Main(string[] args)
         {
-            char z1 = '1';
-            char z2 = '2';
-            char z3 = '3';
+            char z1 = 'X';
+            char z2 = 'Y';
+            char z3 = 'Z';
             Console.WriteLine(z3);
             Console.WriteLine(z2);
             Console.WriteLine(z1);
