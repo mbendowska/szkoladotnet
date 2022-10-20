3e0e603a334cbe71dfa824917a723154f8e6ea9c
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 12:43:58 +0200

    W2L4Ex1 - gotowe

--- Week2/Lesson4/Ex1/W2L4E1/W2L4E1/Program.cs
+++ Week2/Lesson4/Ex1/W2L4E1/W2L4E1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L4Ex1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string imie = "Joanna";
+            string nazwisko = "Styczeń";
+            byte wiek = 23;
+            char plec = 'k';
+            string pesel = "99062702455";
+            long nrprac = 2509324094;
+            Console.WriteLine($"Imię:\t\t{imie}");
+            Console.WriteLine($"Nazwisko:\t{nazwisko}");
+            Console.WriteLine($"Wiek:\t\t{wiek}");
+            if ((plec == 'k') || (plec == 'K'))
+            {
+                Console.WriteLine("Płeć:           KOBIETA");
+            }
+            else
+            {
+                Console.WriteLine("Płeć:           MĘŻCZYZNA");
+            }
+            Console.WriteLine($"Pesel:\t\t{pesel}");
+            Console.WriteLine($"Nr pracownika:\t{nrprac}");
+        }
+    }
+
+}
