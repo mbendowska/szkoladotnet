e619fe50359482e3e6a96227ac6c90933c7db0cb
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 22:38:05 +0200

    W2L7Ex5 - gotowe

---
+++ Week2/Lesson7/Ex5/W2L7E5/W2L7E5/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Napisz program w C#, który sprawdzi czy podany przez użytkownika wiek uprawnia go do ubiegania się o stanowisko posła, premiera, sentarora, prezydenta.
+Dane testowe : 21
+Rezultat w terminalu :
+Możesz zostać posłem
+ */
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wiek");
+            int age = int.Parse(Console.ReadLine());
+
+            if ((age < 21)&&(age>=18))
+            {
+                Console.WriteLine("Możesz brać udział w wborach");
+            }
+            else
+            {
+                if (age >= 21)
+                {
+                    Console.WriteLine("Możesz zostać posłem");
+                    Console.WriteLine("Możesz zostać premierem");
+                }
+                if (age >= 30)
+                {
+                    Console.WriteLine("Możesz zostać senatorem");
+                }
+                if (age >= 35)
+                {
+                    Console.WriteLine("Możesz zostać prezydentem");
+                }
+                if (age < 18)
+                {
+                    Console.WriteLine("Jesteś jeszcze dzieckiem");
+                }
+            }
+        }
+    }
+}
