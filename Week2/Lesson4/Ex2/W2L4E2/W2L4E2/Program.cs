5ea8d3ef2003bfd16c0b970575ad5739725ad9a5
mbendowska <magdalena.bendowska@gmail.com>
2022-10-20 15:55:43 +0200

    W2L4Ex3 - gotowe

--- Week2/Lesson4/Ex2/W2L4E2/W2L4E2/Program.cs
+++ Week2/Lesson4/Ex2/W2L4E2/W2L4E2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L4Ex2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char z1 = '1';
+            char z2 = '2';
+            char z3 = '3';
+            Console.WriteLine(z3);
+            Console.WriteLine(z2);
+            Console.WriteLine(z1);
+        }
+    }
+}
--- Week2/Lesson4/Ex3/W2L4E3/W2L4E3/Program.cs
+++ Week2/Lesson4/Ex3/W2L4E3/W2L4E3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L4E3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość prostokąta");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj szerokość prostokąta");
+            double b = double.Parse(Console.ReadLine());
+
+            double Result = Math.Sqrt(Math.Pow(a,2) + Math.Pow(b,2));
+
+            Console.WriteLine($"Długość przekątnej prostokąta o bokach {a} i {b} wynosi {Result.ToString()}");
+        }
+    }
+}
