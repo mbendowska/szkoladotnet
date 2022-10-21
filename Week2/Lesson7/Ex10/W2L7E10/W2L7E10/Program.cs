a95e1e59c672588afa19be84f676db7082a81aed
mbendowska <magdalena.bendowska@gmail.com>
2022-10-21 12:36:01 +0200

    W2L7Ex10 - gotowe

---
+++ Week2/Lesson7/Ex10/W2L7E10/W2L7E10/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Napisz program, który sprawdzi, czy z 3 podanych długości można stworzyć trójkąt 
+Dane testowe : 40 55 65 
+Rezultat w terminalu : Można zbudować trójkąt
+*/
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość pierwszego boku");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj długość drugiego boku");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj długość trzeciego boku");
+            double c = double.Parse(Console.ReadLine());
+
+            double biggest = 0;
+            double sumsides = 0;
+
+            if ((a > b) && (a > c))
+            {
+                biggest = a;
+                sumsides = b + c;
+            }
+            else if (b > c)
+                 {
+                    biggest = b;
+                    sumsides = a + c;
+                 }
+                 else
+                 {
+                    biggest = c;
+                    sumsides = a + b;
+                 }
+            if (sumsides > biggest)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine($"Trójkąt można zbudować z boków o długościach {a}, {b}, {c}");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Z boków o długościach {a}, {b}, {c} nie można zbudować trójkąta!!!");
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Week2/Lesson7/Ex11/W2L7E11/W2L7E11/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Napisz program, który zmieni ocenę ucznia na jej opis wg podanej tabeli:
+Ocena Opis
+6 Celujący
+5 Bardzo dobry
+4 Dobry
+3 Dostateczny
+2 Dopuszczający
+1 Niedostateczny 
+
+Dane testowe : 3 
+Rezultat w terminalu : Dostateczny
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj uzyskaną ocenę");
+            byte rate = byte.Parse(Console.ReadLine());        
+            switch(rate)
+                { 
+                    case 1:
+                        Console.WriteLine("Ocena niedostateczna");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ocena dopuszczająca");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ocena dostateczna");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ocena dobra");
+                        break;
+                    case 5:
+                        Console.WriteLine("Ocena bardzo dobra");
+                        break;
+                    case 6:
+                        Console.WriteLine("Ocena celująca");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ocenę możesz uzyskać w skali od 1 do 6. Popraw dane!!!");
+                        Console.ResetColor();
+                    break;
+            }
+        }
+                       
+    }
+}
+
