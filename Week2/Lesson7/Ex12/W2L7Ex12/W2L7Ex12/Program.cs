63dbda7270375f2efe62713e18466a174f0be7bc
mbendowska <magdalena.bendowska@gmail.com>
2022-10-21 12:45:25 +0200

    W2L7Ex12 - gotowe

---
+++ Week2/Lesson7/Ex12/W2L7Ex12/W2L7Ex12/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Napisz program, który pobierze numer dnia tygodnia i wyświetli jego nazwę 
+Dane testowe : 4
+Rezultat w terminalu : Czwartek
+*/
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L7E12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj numer dnia tygodnia");
+            byte idday = byte.Parse(Console.ReadLine());
+            switch (idday)
+            {
+                case 1:
+                    Console.WriteLine("Poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("Wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("Środa");
+                    break;
+                case 4:
+                    Console.WriteLine("Czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("Piątek");
+                    break;
+                case 6:
+                    Console.WriteLine("Sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("Niedziela");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Mamy tylko 7 dni w tygodniu. Możesz wpisać cyfry od 1 do 7!!! Popraw dane!!!");
+                    Console.ResetColor();
+                    break;
+            }
+        }
+    }
+}
