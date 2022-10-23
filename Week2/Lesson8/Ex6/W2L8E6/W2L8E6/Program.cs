44fa6d760f0ec28c002e6d02a8bb76ff7fa5c02f
mbendowska <magdalena.bendowska@gmial.com>
2022-10-23 21:58:18 +0200

    W2L8Ex6 - jeszcze nie działa

---
+++ Week2/Lesson8/Ex6/W2L8E6/W2L8E6/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Napisz program, który dla liczb od 0 do 20 obliczy sumę wg wzoru:
+1 + ½ + 1/3 + ¼ itd.
+*/
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L8E6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double result = 0.0;
+            double temp = 0.0;
+            for (int i = 1; i < 21; i++)
+            {
+                if (i==21)
+                {
+                    Console.WriteLine($"1/{i}");
+                    temp = 1 / i;
+                    result=result+temp;
+                    Console.WriteLine(result);
+                }
+                Console.Write($"1/{i}+");
+                temp = 1 / i;
+                result=result+temp;
+            }
+        }
+    }
+}
