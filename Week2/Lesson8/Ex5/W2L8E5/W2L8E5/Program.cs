9d4564884cebba0ac7df707f1f7c4378846bc999
mbendowska <magdalena.bendowska@gmial.com>
2022-10-23 21:31:15 +0200

    W2L8Ex5 - gotowe

---
+++ Week2/Lesson8/Ex5/W2L8E5/W2L8E5/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Napisz program, który dla liczb od 1 do 20 wyświetli na ekranie ich 3 potęgę.
+*/
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L8E5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {         
+            for (long i = 1; i < 21; i++)
+            {
+                Console.WriteLine($"{i}^3={i*i*i}");
+            }
+        }
+    }
+}
