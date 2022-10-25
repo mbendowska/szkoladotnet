6d2a9ea0821d9b07065122ef3685e4d58b4d48a1
mbendowska <magdalena.bendowska@gmail.com>
2022-10-25 08:33:43 +0200

    W2L8Ex9 - gotowe

---
+++ Week2/Lesson8/Ex7/W2L8E7/W2L8E7/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Napisz program, który dla liczby zadanej przez użytkownika narysuje diament o krótszej przekątnej o długości wprowadzonej przez użytkownika i wg wzoru: 
+  Dla liczby 9 wygląda to tak:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+//Zadanie 7
+int p;
+Console.WriteLine("Podaj przekątną:");
+Int32.TryParse(Console.ReadLine(), out p);
+
+for (int i = 0; i < p; i++)
+{
+    for (int j = 0; j < (p - i); j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+for (int i = p - 2; i >= 1; i--)
+{
+    for (int j = 0; j < (p - i); j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
