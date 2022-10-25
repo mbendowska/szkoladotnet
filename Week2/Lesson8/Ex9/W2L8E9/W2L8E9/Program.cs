c019c71029efd8c42cea053fb95276568a6e05dd
mbendowska <magdalena.bendowska@gmail.com>
2022-10-25 08:33:32 +0200

    W2L8Ex9 - gotowe

---
+++ Week2/Lesson8/Ex9/W2L8E9/W2L8E9/Program.cs
@@ -0,0 +1,25 @@
+﻿// Napisz program, który zamieni liczbę dziesiętną na liczbę binarną
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L8E9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj całkowitą liczbę dziesiętną");
+            int number = int.Parse(Console.ReadLine());
+            string result = "";
+            while (number>0)
+            {
+                int temp=number % 2;
+                result = temp + result;
+                number= number/2;               
+            }
+            Console.WriteLine($"{result}");
+            //Console.ReadKey();
+        }
+    }
+}
+
