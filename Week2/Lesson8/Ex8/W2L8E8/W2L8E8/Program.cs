7ed536155a7a022ea98d1d0dd68a5d4e92214111
mbendowska <magdalena.bendowska@gmail.com>
2022-10-25 07:24:10 +0200

    W2L8Ex8 - gotowe

---
+++ Week2/Lesson8/Ex8/W2L8E8/W2L8E8/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Napisz program, który odwróci wprowadzony przez użytkownika ciąg znaków. Np.
+Testowe dane:
+Abcdefg
+Rezultat
+Gfedcba*/
+using System;
+using System.Runtime.CompilerServices;
+
+namespace W2L8E8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ciąg znaków, które mają zostać obrócone");
+            string task = Console.ReadLine();
+            for (int i = task.Length; i > 0 ; i--)
+            {
+                char temp =  task[i-1];
+                Console.Write($"{temp}");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
