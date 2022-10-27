b1a06f088d9ccdbe6dd63f717f8a6fa6141023d0
mbendowska <magdalena.bendowska@gmail.com>
2022-10-27 16:29:23 +0200

    W2L8E10 - gotowe

---
+++ Week2/Lesson8/Ex10/W2L8Ex10/W2L8Ex10/Program.cs
@@ -0,0 +1,37 @@
+﻿// Napisz program, który znajdzie najmniejszą wspólną wielokrotność dla zadanych 2 liczb
+
+using Microsoft.VisualBasic;
+using System;
+using System.Runtime.CompilerServices;
+using System.Xml.XPath;
+
+namespace W2L8E9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę");
+            int second = int.Parse(Console.ReadLine());
+
+            int result = first * second;
+
+            while (first!=second)
+            {
+                if (first > second)
+                {
+                    first = first - second;
+                }
+                else
+                {
+                    second = second - first;
+                }
+            }
+            result = result / second;
+            Console.WriteLine($"{result}");
+            //Console.ReadKey();
+        }
+    }
+}
